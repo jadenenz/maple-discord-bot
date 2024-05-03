0956bc25be6a12cb3c644d13e5c5b5203d82d799
Jaden <jenzbr@gmail.com>
2024-05-03 14:54:40 -0500

    refactored fetch exp function and added request batching

--- commands/utility/exp.js
+++ commands/utility/exp.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require("discord.js")
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js")
 const { fetchExp } = require("./../../helpers/fetchExp")
 
 module.exports = {
@@ -6,7 +6,64 @@ module.exports = {
     .setName("exp")
     .setDescription("Fetches exp for the set list of IGNs"),
   async execute(interaction) {
-    const sortedExp = await fetchExp()
-    await interaction.reply(`Sorted EXP: ${JSON.stringify(sortedExp)}`)
+    try {
+      const ignList = [
+        "wizyori",
+        "jeraie",
+        "shlop",
+        "traemon",
+        "meleeking",
+        "kinkflip",
+        "salary",
+        "plimbotoilet",
+        "camperdown",
+        "outer",
+        "boredy",
+        "biepo",
+        "moonlemon",
+      ]
+      await interaction.deferReply()
+      const sortedExp = await fetchExp(ignList)
+
+      // const expEmbed = new EmbedBuilder()
+      //   .setColor(0x0099ff)
+      //   .setTitle("Flurm")
+      //   .setDescription("Exp fetched from MapleRanks")
+      //   .addFields(
+      //     {
+      //       name: `1st: ${sortedExp[0].name}`,
+      //       value: sortedExp[0].number,
+      //     },
+      //     { name: `2nd: ${sortedExp[1].name}`, value: sortedExp[1].number }
+      //   )
+      //   .setTimestamp()
+      //   .setFooter({ text: "Meow" })
+
+      const expEmbed = {
+        color: 0x0099ff,
+        title: "Flurm105",
+        description: "Exp fetched from MapleRanks",
+        fields: [
+          // {
+          //   name: `1st: ${sortedExp[0].name}`,
+          //   value: sortedExp[0].number,
+          // },
+          // { name: `2nd ${sortedExp[1].name}`, value: sortedExp[1].number },
+          // { name: `3rd ${sortedExp[2].name}`, value: sortedExp[2].number },
+        ],
+        timestamp: new Date().toISOString(),
+      }
+
+      sortedExp.forEach((element) => {
+        expEmbed.fields.push({
+          name: element.name,
+          value: element.number,
+        })
+      })
+
+      await interaction.editReply({ embeds: [expEmbed] })
+    } catch (error) {
+      console.error(error)
+    }
   },
 }
--- helpers/fetchExp.js
+++ helpers/fetchExp.js
@@ -1,79 +1,102 @@
 const puppeteer = require("puppeteer")
 
-async function fetchExp() {
-  const ignList = ["wizyori", "jeraie", "shlop", "traemon", "meleeking"]
-
-  const getRates = async (ign) => {
-    // Start a Puppeteer session with:
-    // - a visible browser (`headless: false` - easier to debug because you'll see the browser in action)
-    // - no default viewport (`defaultViewport: null` - website page will in full width and height)
-    const browser = await puppeteer.launch({
-      headless: false,
-      defaultViewport: null,
-    })
-
-    // Open a new page
-    const page = await browser.newPage()
-
-    // On this new page
-    // - open mapleranks to specified char
-    // - wait until the dom content is loaded (HTML is ready)
-    await page.goto(`https://mapleranks.com/u/${ign}`, {
-      waitUntil: "domcontentloaded",
-    })
-
-    // Get page data
-    const rates = await page.evaluate(() => {
-      const name = document.querySelector(".card-title").innerText
-      const expValue = document.querySelector(".char-exp-cell")
-
-      const description = expValue.innerText.split("\n")[0]
-      const number = expValue.querySelector(".char-stat-right").innerText
-
-      return { name, description, number }
-    })
-
-    // Display the page data
-    // console.log(rates)
-
-    // Close the browser
-    await browser.close()
-
-    return rates
+async function fetchExp(ignList) {
+  const batchSize = 5
+  const batches = []
+  for (let i = 0; i < ignList.length; i += batchSize) {
+    batches.push(ignList.slice(i, i + batchSize))
   }
 
-  const playerListWithData = ignList.map((player) => {
-    return getRates(player)
-  })
+  const playerListWithData = []
 
-  const sortedResult = await Promise.all(playerListWithData).then((values) => {
-    let newArr = [].concat(...values) // Flatten the array of arrays into a single array
-    const sorted = newArr.sort((a, b) => {
-      const numberA = parseNumber(a.number)
-      const numberB = parseNumber(b.number)
-      return numberB - numberA
-    })
-    return sorted
-  })
-
-  function parseNumber(numberString) {
-    const numericPart = parseFloat(numberString.replace(/[^\d.-]/g, ""))
-    const unit = numberString.slice(-1) // Get the last character (unit)
-
-    switch (unit) {
-      case "M":
-        return numericPart * 1e6 // Convert million to its numerical equivalent
-      case "B":
-        return numericPart * 1e9 // Convert billion to its numerical equivalent
-      case "T":
-        return numericPart * 1e12 // Convert trillion to its numerical equivalent
-      default:
-        return numericPart // Return as is if no unit or unrecognized unit
-    }
+  for (const batch of batches) {
+    const batchResults = await Promise.all(
+      batch.map((player) => getRates(player))
+    )
+    playerListWithData.push(...batchResults)
+    await delay(10000)
   }
+
+  const sortedResult = playerListWithData.sort((a, b) => {
+    const numberA = parseNumber(a.number)
+    const numberB = parseNumber(b.number)
+    return numberB - numberA
+  })
 
   return sortedResult
 }
+
+async function getRates(ign) {
+  // Start a Puppeteer session with:
+  // - a visible browser (`headless: false` - easier to debug because you'll see the browser in action)
+  // - no default viewport (`defaultViewport: null` - website page will in full width and height)
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+  })
+
+  // Open a new page
+  const page = await browser.newPage()
+
+  // On this new page
+  // - open mapleranks to specified char
+  // - wait until the dom content is loaded (HTML is ready)
+  await page.goto(`https://mapleranks.com/u/${ign}`, {
+    waitUntil: "domcontentloaded",
+  })
+
+  // Get page data
+  const rates = await page.evaluate(() => {
+    const name = document.querySelector(".card-title").innerText
+    const expValue = document.querySelector(".char-exp-cell")
+
+    const description = expValue.innerText.split("\n")[0]
+    const number = expValue.querySelector(".char-stat-right").innerText
+
+    return { name, description, number }
+  })
+
+  // Display the page data
+  // console.log(rates)
+
+  // Close the browser
+  await browser.close()
+
+  return rates
+}
+
+function parseNumber(numberString) {
+  const numericPart = parseFloat(numberString.replace(/[^\d.-]/g, ""))
+  const unit = numberString.slice(-1) // Get the last character (unit)
+
+  switch (unit) {
+    case "M":
+      return numericPart * 1e6 // Convert million to its numerical equivalent
+    case "B":
+      return numericPart * 1e9 // Convert billion to its numerical equivalent
+    case "T":
+      return numericPart * 1e12 // Convert trillion to its numerical equivalent
+    default:
+      return numericPart // Return as is if no unit or unrecognized unit
+  }
+}
+
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+const ignList = [
+  "wizyori",
+  "jeraie",
+  "shlop",
+  "traemon",
+  "meleeking",
+  "kinkflip",
+  "salary",
+  // "plimbotoilet",
+]
+
+// fetchExp(ignList)
 
 module.exports = {
   fetchExp,
